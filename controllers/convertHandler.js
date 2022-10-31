0ee7002e06a97ee0bac016248846c2bae350afcf
Andrew Toussaint <ajntoussaint@gmail.com>
2022-10-31 17:28:56 -0400

    Added test route, comments, and console logs

--- controllers/convertHandler.js
+++ controllers/convertHandler.js
@@ -1,44 +1,57 @@
 function ConvertHandler() {
-  
+
   this.getNum = function(input) {
+    //takes query string as an input and returns the number as float
+    console.log("using getNum");
     let result;
-    
+
     return result;
   };
-  
+
   this.getUnit = function(input) {
+    //Takes Query string as input and returns the unit as string
+    console.log("using getUnit");
     let result;
-    
+
     return result;
   };
-  
+
   this.getReturnUnit = function(initUnit) {
+    //takes the return of "getUnit" and returns the converted unit as string
+    //Ex if getUnit(...)=gal then this will return "L"
+    console.log("using getReturnUnit");
     let result;
-    
+
     return result;
   };
 
   this.spellOutUnit = function(unit) {
+    //takes abbreiviation as input and reutrns full name as str
+    console.log("using spellOutUnit");
     let result;
-    
+
     return result;
   };
-  
+
   this.convert = function(initNum, initUnit) {
+    //takes initial number and unit from query string
+    console.log("using convert");
     const galToL = 3.78541;
     const lbsToKg = 0.453592;
     const miToKm = 1.60934;
     let result;
-    
+
     return result;
   };
-  
+
   this.getString = function(initNum, initUnit, returnNum, returnUnit) {
+    //takes all of the information and returns a string that describes the conversion
+    console.log("using get String");
     let result;
-    
+
     return result;
   };
-  
+
 }
 
 module.exports = ConvertHandler;
--- routes/api.js
+++ routes/api.js
@@ -4,7 +4,15 @@ const expect = require('chai').expect;
 const ConvertHandler = require('../controllers/convertHandler.js');
 
 module.exports = function (app) {
-  
+
   let convertHandler = new ConvertHandler();
+
+  //A test route to help me understand
+  app.route("/api/convert").get( (req,res) => {
+    //end goal: take the query and return a JSON object with all the info in the example
+    //Ex: { initNum: 3.1, initUnit: 'mi', returnNum: 4.98895, returnUnit: 'km', string: '3.1 miles converts to 4.98895 kilometers' }
+    console.log("test route");
+    res.send("Hello API")
+  })
 
 };
